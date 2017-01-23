e51bd2327d834d25872fe733e21f3e2328a4fc46
Magnus Germundsson <a16magge@hs.local>
2017-01-23 14:49:33 +0100

    Added a simple playable object
    Added a red square for the object
    Added a script file for the object
    Placed the object in the main scene

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0.0f, Input.GetAxis("Vertical") * Time.deltaTime, 0.0f));
+	}
+}
